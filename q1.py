0b166801d8694753cdcef982adcbbf5e86ab61fb
leefige <yifeili3@cs.cmu.edu>
2020-12-26 21:24:46 +0800

    Use npz for q1.

--- q1.py
+++ q1.py
@@ -8,7 +8,7 @@ import numpy as np
 from erw import ERW
 
 ITERS = 100
-BOUND = 2e4
+BOUND = 1e4
 TMP_DIR = "tmp_q1"
 
 class TooDeepError(RuntimeError):
@@ -26,20 +26,21 @@ def sim(p, q, A):
     return elephant.steps()
 
 def A_func(A):
-    with open(f"{TMP_DIR}/{A}.csv", 'w', encoding='utf-8') as fout:
-        for p_ in range(0, 11):
-            p = p_ / 10
-            for q_ in range(0, 11):
-                q = q_ / 10
-                print(f"{p:.2f}, {q:.2f}, {A:d}")
-                res = []
-                for i in (range(ITERS)):
-                    try:
-                        res.append(sim(p, q, A))
-                    except TooDeepError:
-                        pass
-                fout.write(f"{p:.1f},{q:.1f},{A:d},{np.mean(res):.2f},{np.std(res):.2f},{len(res)/ITERS*100:.2f}\n")
-                fout.flush()
+    # with open(f"{TMP_DIR}/{A}.csv", 'w', encoding='utf-8') as fout:
+    output = {}
+    for p_ in range(0, 11):
+        p = p_ / 10
+        for q_ in range(0, 11):
+            q = q_ / 10
+            print(f"{p:.2f}, {q:.2f}, {A:d}")
+            res = []
+            for i in (range(ITERS)):
+                try:
+                    res.append(sim(p, q, A))
+                except TooDeepError:
+                    pass
+            output[f'{p_}_{q_}_{A}'] = np.array(res)
+    np.savez(f"{TMP_DIR}/{A}.npz", **output)
 
 def worker_func(As):
     for A in As:
@@ -72,15 +73,16 @@ if __name__ == '__main__':
     for p in procs:
         p.join()
 
-    # merge
-    with open("q1.csv", 'w') as fout:
-        fout.write("p,q,A,mean,std,succ\n")
-        fout.flush()
-        for A in range(-10, 11, 2):
-            with open(f"{TMP_DIR}/{A}.csv", 'r', encoding='utf-8') as fin:
-                lines = fin.readlines()
-                fout.writelines(lines)
-                fout.flush()
+    # merge    
+    all_output = {}
+    for A in range(-10, 11, 2):
+        f = np.load(f"{TMP_DIR}/{A}.npz")
+        for k in f.files:
+            print(f"{k} loaded")
+            all_output[k] = f[k]
+        f.close()
+
+    np.savez_compressed("q1.npz", all_output)
 
     if os.path.isdir(TMP_DIR):
         shutil.rmtree(TMP_DIR)
